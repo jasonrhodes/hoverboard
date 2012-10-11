44b5bb64836036e1f4941780059cd3eb4a5abf81
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2012-10-11 15:14:45 -0400

    Data miner additions

--- hoverboard/workers/DrupalDataMiner.php
+++ hoverboard/workers/DrupalDataMiner.php
@@ -4,64 +4,86 @@ namespace hoverboard\workers;
 
 class DrupalDataMiner
 {
+	public static $targets = array(
+		"safe_value" => "", 
+		"value" => "", 
+		"target_id" => "", 
+		"tid" => "", 
+		"fid" => ""
+	);
+
 	public function dig(array $data)
 	{
-		$diamonds = array();
+		$dug = array();
 
 		foreach ($data as $node) {
-			$diamond = new \stdClass;
 			foreach ($node as $key => $value) {
-				$diamond->$key = $this->extract($value);
+				// echo $key . "<br>";
+				$dug[$this->cleanKey($key)] = $this->extract($value);
 			}
-			$diamonds[] = $diamond;
 		}
-		
-		return $diamonds;
+		return $dug;
 	}
 
-    protected function extract($value)
-    {
-    	// text field labels we want to grab in order of precedence
-    	$textLabels = array("safe_value", "value", "target_id");
-    	$termLabel = "tid";
-    	$fileLabel = "fid";
-
-    	if (!is_array($value)) {
-    		return $value;
-    	}
-
-		// get inside the "und" array where all the goods are
-		$value = $value["und"];
-
-		// text field
-		if (count($value == 1) && !isset($value[0][$fileLabel]) && !isset($value[0][$termLabel])) {
-
-			$value = $value[0];
-
-			// loop through our prefered labels until we find a match
-			foreach ($textLabels as $label) {
-				if (isset($value[$label])) {
-					return $value[$label];
-				}
-			}
-
-		// file(s)
-		} elseif (isset($value[0]["fid"])) {
-			$files = array();
-			foreach($value as $k => $v) {
-				$files[] = $v["fid"];
-			}
-			return $files;
-
-		// image(s)
-		} elseif (isset($value[0]["tid"])) {
-			$terms = array();
-			foreach($value as $k => $v) {
-				$terms[] = $v["tid"];
-			}
-			return $terms;
-		} else {
-			return "we missed something.";
+	public function cleanKey($key)
+	{
+		if (substr($key, 0, 6) == "field_") {
+			$key = substr($key, 6);
 		}
-    }
+
+		if ($key == "nid") {
+			$key = "id";
+		}
+
+		if ($key == "title") {
+			$key = "slug";
+		}
+
+		if ($key == "created") {
+			$key = "publish_date";
+		}
+
+		if ($key == "related_items") {
+			$key = "related_articles";
+		}
+
+		if ($key == "summary") {
+			$key = "excerpt";
+		}
+
+		return $key;
+	}
+
+	public function extract($value, &$parent = array())
+	{
+		// Return the value if it's not an array
+		if (!is_array($value)) {
+			return $value;
+		}
+
+		// Find any keys in the current level of the array that match our targeted keys
+		$matching = array_keys(array_intersect_key(static::$targets, $value));
+
+		// If we found any matching target keys
+		if (!empty($matching)) {
+			// If the parent array has more than one child (ie this value has siblings)
+			if (count($parent) > 1) {
+				$results = array();
+				// Return all of the matching target values in each of the siblings
+				foreach ($parent as $child) {
+					$results[] = $child[$matching[0]];
+				}
+				return $results;
+			} else {
+				// Otherwise just return this matching value
+				return $value[$matching[0]];
+			}
+		} else {
+			// Save off the current array to pass as the parent
+			$parent = $value;			
+			// Recursively call this function on the next child array
+			return $this->extract(array_shift($value), $parent);
+		}
+	}
+
 }
